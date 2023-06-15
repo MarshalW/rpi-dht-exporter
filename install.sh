230e4006b59b0ac98bff4413d2ee67501b4a7463
marshalw <marshal.wu@gmail.com>
2023-06-15 17:29:21 +0800

    update install

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ set -e
 
 sudo cp -r . /opt/dht-exporter
 sudo cp dht.service /etc/systemd/system/dht.service
+
+# 启动
+sudo systemctl daemon-reload 
+sudo systemctl enable dht.service
+sudo systemctl start dht.service
