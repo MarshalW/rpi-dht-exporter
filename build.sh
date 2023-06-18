6b84b27dad69743f9af462d1db1478b45ed8a739
marshalw <marshal.wu@gmail.com>
2023-06-18 23:09:31 +0800

    update

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-docker buildx use mybuilder
+# docker buildx create --name mybuilder --use --bootstrap
+# docker buildx use mybuilder
+
+docker buildx create --platform linux/arm64 --name rpibuilder --use
 
 docker buildx build --push \
 --platform linux/arm64 \
 --tag marshalw/rpi-dht-exporter:latest .
+
+docker buildx rm rpibuilder
